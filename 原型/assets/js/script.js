2891516cd90155e91934de7180b57d6544671cdf
vboar <vboar@live.com>
2015-10-28 13:02:40 +0800

    原型

---
+++ 原型/assets/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Vboar on 2015/10/22.
+ */
+$(document).ready(function () {
+
+    // 下拉菜单自动展开收回
+    $("li.dropdown").mouseover(function() {
+        $(this).addClass('open');
+    }).mouseout(function() {
+        $(this).removeClass('open');
+    });
+
+
+});
